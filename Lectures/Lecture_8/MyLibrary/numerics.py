ed161a1a41b4a073edb19f6cfde85ef3422b7c4e
RossCZ <RossCZ@bitbucket.org>
2021-09-12 14:52:14 +0200

    lectures - naming

--- Lectures/Lecture_8/MyLibrary/numerics.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def is_less(a, b):
-    print("module numerika")
-    return a < b
--- Lectures/Lecture_9/data_load_basic.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("portfolio_data.csv")
-plt.plot(data["AMZN"])
-plt.show()
-
-
-

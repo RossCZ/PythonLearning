1426a23806d80fb9b252fc909751dfe15930d31c
RossCZ <rostislav.krc@seznam.cz>
2022-11-26 22:54:00 +0100

    feat: string split, data analysis example

--- topics/1_basics/5_string_split.py
+++ topics/1_basics/5_string_split.py
@@ -1,11 +1,22 @@
-txt = "a b c d"
-txt2 = "a-b-c-d"
+def example1():
+    txt = "a b c d"
+    txt2 = "a-b-c-d"
 
-lst = txt.split(" ")
-print(lst)
+    lst = txt.split(" ")
+    print(lst)
 
-lst2 = txt2.split("-")
-for character in lst2:
-    print(character)
+    lst2 = txt2.split("-")
+    for character in lst2:
+        print(character)
 
 
+def example2():
+    data = "1,25.6,-8.3"
+    print(data[1])
+
+    data_splitted = data.split(",")
+    print(data_splitted[1])
+
+
+example1()
+example2()
